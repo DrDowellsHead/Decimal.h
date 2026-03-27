a70595d3b23096ac4f74acc267dbd0a709132e48
Phung Sticks <phungsti@ge-d7.nsk.21-school.ru>
2026-03-27 16:35:34 +0700

    first step

---
+++ src/DecTest.c
@@ -0,0 +1,15 @@
+#include "s21_decimal.h"
+#include "s21_auxfunct.h"
+
+int main() {
+  s21_decimal s21Numb = {{-1, -2, -3, 1}};
+  printDecimalBits(s21Numb);
+
+  int bitN = 31;
+  while (bitN < 128) {
+    scanf("%d", &bitN);
+    getDecimalBit(s21Numb, bitN);
+  }
+
+  return 0;
+}
